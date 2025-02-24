4bad96db8b44bfc58deb0da6eb3fd69f3e54e826
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-02-24 14:02:42 +0530

    Add YAML support and enhance RDSFileStore functionality - Add CRUD Methods      create, read, update ,delete - Added Query Methods
    Co-authored-by: Tauquir <30658453+itstauq@users.noreply.github.com>
    Co-authored-by: Shubham Gupta <11032835+shubham3121@users.noreply.github.com>
    Co-authored-by: khoaguin <dkn.work@protonmail.com>
    Co-authored-by: Yash Gorana <yash.gorana@hotmail.com>

--- syft-rds/src/syft_rds/store.py
+++ syft-rds/src/syft_rds/store.py
@@ -1,13 +1,13 @@
 from uuid import uuid4
-from typing import Optional
+from uuid import UUID
+from pathlib import Path
+from typing import Any, Optional
 from syft_core import Client
 from syft_core.url import SyftBoxURL
 from pydantic import BaseModel
-from pydantic import EmailStr
 from pydantic import Field
-from enum import StrEnum
+from functools import wraps
 import yaml
-import json
 
 
 PERMS = """
@@ -20,33 +20,37 @@ PERMS = """
 """
 
 
-class RuntimeType(StrEnum):
-    docker = "docker"
-    # k8s = "k8s"
-    # local = "local"
-
-
 class BaseSpec(BaseModel):
-    __name__: str
-    __indexes__: list[str]
-    _id: str = Field(default_factory=uuid4)
+    __spec_name__: str
+    id: UUID = Field(default_factory=uuid4)
+
+    class Config:
+        arbitrary_types_allowed: bool = True
 
 
 class DatasetSpec(BaseSpec):
-    __name__ = "dataset"
-    __indexes__ = ["name"]
-
+    __spec_name__ = "dataset"
+    
     name: str
     description: str
     data: SyftBoxURL
     mock: SyftBoxURL
     tags: list[str]
 
+class StoreSpecNotFoundError(FileNotFoundError):
+    pass
+
 
 # class CodeSpec(Base):
 #     language: str
 #     path: SyftBoxURL
 #     created_by: EmailStr
+
+
+# class RuntimeType(StrEnum):
+#     docker = "docker"
+#     # k8s = "k8s"
+#     # local = "local"
 
 
 # class RuntimeSpec(Base):
@@ -74,12 +78,20 @@ class DatasetSpec(BaseSpec):
 #     environment: str
 
 
-class RDSStore:
-    specs: list = [DatasetSpec]  # , CodeSpec, RuntimeSpec, JobSpec]
+def ensure_spec_store_exists(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not self.spec_store_path.exists():
+            self.spec_store_path.mkdir(parents=True, exist_ok=True)
+        return func(self, *args, **kwargs)
+    return wrapper
 
+
+class RDSFileStore:
     def __init__(
         self,
         app_name: str,
+        spec: BaseSpec,
         client: Optional[Client] = None,
     ):
         self.app_name = app_name
@@ -87,38 +99,92 @@ class RDSStore:
             self.client = Client.load()
         self.app_dir = self.client.api_data(self.app_name)
         self.app_store_dir = self.app_dir / "store"
-
-        # Create app store directory
-        self.app_store_dir.mkdir(exist_ok=True, parents=True)
+        self.spec = spec
 
         # Create restrictive Permissions for the store Directory
         perms = self.app_store_dir / "syftperm.yaml"
         perms.write_text(PERMS)
 
-        # index file
-        for spec in self.specs:
-            index_file_path = self.app_store_dir / f"{spec.__name__}.json"
-            index_file_path.write_text("\{\}")
 
     @property
-    def store_dir(self):
+    def store_dir(self) -> Path:
         return self.app_store_dir
 
-    def _update_index(self, spec: BaseSpec):
-        index_file_path = self.app_store_dir / f"{spec.__name__}.json"
-        index = json.loads(index_file_path.read_text())
-        spec_indexes = spec.__indexes__
-        for idx in spec_indexes:
-            key = getattr(spec, idx)
-            if key not in index:
-                index[key] = set()
-            index[key].add(spec._id)
-        index_file_path.write_text(json.dumps(index))
+    @property
+    def spec_store_path(self) -> Path:
+        return self.store_dir / self.spec.__spec_name__
 
-    def create(self, spec: BaseSpec) -> str:
-        file_path = self.app_store_dir / spec.__name__ / f"{spec._id}.yaml"
-        json_dump = spec.model_dump_json()
-        yaml_dump = yaml.dump(json_dump)
+    def _get_record_path(self, id: str | UUID) -> Path:
+        """Get the full path for a record's YAML file from it's ID."""
+        return self.spec_store_path / f"{id}.yaml"
+
+    def _save_record(self, record: BaseSpec) -> None:
+        """Save a single record to its own YAML file"""
+        file_path = self._get_record_path(record.id)
+        yaml_dump = yaml.safe_dump(record.model_dump(mode="json"), indent=2)
         file_path.write_text(yaml_dump)
-        self._update_index(spec)
-        return spec._id
+
+    def _load_record(self, id: str | UUID) -> BaseSpec:
+        """Load a single record from its own YAML file"""
+        file_path = self._get_record_path(id)
+        if not file_path.exists():
+            return None
+        record_dict = yaml.safe_load(file_path.read_text())
+        return self.spec.model_validate(**record_dict)
+
+    def _list_all_records(self) -> list[BaseSpec]:
+        """List all records in the store"""
+        records= []
+        for file_path in self.spec_store_path.glob("*.yaml"):
+            _id = file_path.stem
+            records.append(self._load_record(_id))
+        return records
+
+    @ensure_spec_store_exists
+    def create(self, record: BaseSpec) -> str:
+        if not isinstance(record, self.spec):
+            raise TypeError(f"`record` must be of type {self}")
+        record.id = uuid4()  # Generate a new id during "create"
+        self._save_record(record)
+        return record.id
+
+    @ensure_spec_store_exists
+    def read(self, id: str | UUID) -> BaseSpec:
+        return self._load_record(id)
+
+    @ensure_spec_store_exists
+    def update(self, id: str | UUID, item: BaseSpec) -> BaseSpec|None:
+        existing_record = self._load_record(id)
+        if not existing_record:
+            return None
+
+        # Update the record
+        record = {
+            **existing_record.model_dump_json(),
+            **item.model_dump_json(),
+            "id": id
+        }
+        self._save_record(record)
+        return self._load_record(id)
+
+    @ensure_spec_store_exists
+    def delete(self, id: str | UUID) -> bool:
+        file_path = self._get_record_path(id)
+        if not file_path.exists():
+            return False
+        file_path.unlink()
+        return True
+
+    @ensure_spec_store_exists
+    def query(self, **filters) -> list[BaseSpec]:
+        results = []
+
+        for record in self._list_all_records():
+            matches = True
+            for key, value in filters.items():
+                if key not in record or getattr(record, key) != value:
+                    matches = False
+                    break
+            if matches:
+                results.append(record)
+        return results
