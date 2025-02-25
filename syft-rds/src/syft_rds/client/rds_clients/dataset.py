f852fa797c7c7fd24b059f5762cbbe8107818c82
dk <88959106+khoaguin@users.noreply.github.com>
2025-02-25 11:48:54 +0700

    refactor DatasetRDSClient

--- syft-rds/src/syft_rds/client/rds_clients/dataset.py
+++ syft-rds/src/syft_rds/client/rds_clients/dataset.py
@@ -14,24 +14,198 @@ from syft_rds.models.models import (
 )
 
 
+class DatasetUrlManager:
+    @staticmethod
+    def get_syftbox_mock_dataset_url(
+        datasite_email: str, dataset_name: str, mock_path: Union[Path, str]
+    ) -> str:
+        return f"syft://{datasite_email}/public/datasets/{dataset_name}/{Path(mock_path).name}"
+
+    @staticmethod
+    def get_syftbox_private_dataset_url(
+        dataset_name: str, path: Union[Path, str]
+    ) -> str:
+        return f"syft://private/datasets/{dataset_name}/{Path(path).name}"
+
+
+class DatasetPathManager:
+    def __init__(self, syftbox_client: SyftBoxClient):
+        self._syftbox_client = syftbox_client
+
+    def get_syftbox_public_dataset_dir(self, dataset_name: str) -> Path:
+        return self._syftbox_client.my_datasite / "public" / "datasets" / dataset_name
+
+    def get_syftbox_private_dataset_dir(self, dataset_name: str) -> Path:
+        return (
+            self._syftbox_client.workspace.data_dir
+            / "private"
+            / "datasets"
+            / dataset_name
+        )
+
+    @property
+    def syftbox_client_email(self) -> str:
+        return self._syftbox_client.email
+
+    def check_path_exists(self, path: Union[str, Path]):
+        if not Path(path).exists():
+            raise ValueError(f"Paths must exist: {path}")
+
+    def check_are_both_dirs_or_files(
+        self, path: Union[str, Path], mock_path: Union[str, Path]
+    ):
+        path, mock_path = Path(path), Path(mock_path)
+        if not (
+            (path.is_dir() and mock_path.is_dir())
+            or (path.is_file() and mock_path.is_file())
+        ):
+            raise ValueError(f"Paths must be same type: {path} and {mock_path}")
+
+
+class DatasetFilesManager:
+    def __init__(self, path_manager: DatasetPathManager) -> None:
+        self._path_manager = path_manager
+
+    def check_same_file_extensions(
+        self, path: Union[str, Path], mock_path: Union[str, Path]
+    ):
+        path, mock_path = Path(path), Path(mock_path)
+        if (path.is_file() and mock_path.is_file()) and (
+            not path.suffix == mock_path.suffix
+        ):
+            raise ValueError(f"Files must have same extension: {path} and {mock_path}")
+
+    def check_file_extensions_for_dir(self, path: Union[str, Path], file_type: str):
+        """
+        Check all the file extensions in the dir and compare if they are in the list of file extensions
+        """
+        pass
+
+    @staticmethod
+    def _copy_file_or_dir(src: Union[str, Path], dest_dir: Path) -> Path:
+        src_path = Path(src)
+        dest_dir.mkdir(parents=True, exist_ok=True)
+        dest_path = dest_dir / src_path.name
+
+        if src_path.is_dir():
+            shutil.copytree(src_path, dest_path, dirs_exist_ok=True)
+        else:
+            shutil.copy2(src_path, dest_path)
+        return dest_path
+
+    def copy_mock_to_public_syftbox_dir(
+        self, dataset_name: str, mock_path: Union[str, Path]
+    ) -> Path:
+        public_dataset_dir: Path = self._path_manager.get_syftbox_public_dataset_dir(
+            dataset_name
+        )
+        return self._copy_file_or_dir(mock_path, public_dataset_dir)
+
+    def copy_private_to_private_syftbox_dir(
+        self,
+        dataset_name: str,
+        path: Union[str, Path],
+    ) -> Path:
+        private_dataset_dir: Path = self._path_manager.get_syftbox_private_dataset_dir(
+            dataset_name
+        )
+        return self._copy_file_or_dir(path, private_dataset_dir)
+
+    def copy_desc_file_to_public_syftbox_dir(
+        self, dataset_name: str, description_path: Union[str, Path]
+    ) -> Path:
+        public_dataset_dir: Path = self._path_manager.get_syftbox_public_dataset_dir(
+            dataset_name
+        )
+        dest_path = public_dataset_dir / Path(description_path).name
+        shutil.copy2(description_path, dest_path)
+        return dest_path
+
+    def cleanup_dataset_files(self, name: str) -> None:
+        try:
+            public_dir = self._path_manager.get_syftbox_public_dataset_dir(name)
+            private_dir = self._path_manager.get_syftbox_private_dataset_dir(name)
+            shutil.rmtree(public_dir)
+            shutil.rmtree(private_dir)
+        except Exception as e:
+            logger.error(f"Failed to cleanup dataset files: {str(e)}")
+            raise RuntimeError(f"Failed to clean up dataset '{name}': {str(e)}") from e
+
+
+class DatasetSchemaManager:
+    def __init__(self, path_manager: DatasetPathManager) -> None:
+        self._path_manager = path_manager
+
+    def generate_dataset_schema(self, dataset_create: DatasetCreate) -> None:
+        mock_path = Path(dataset_create.mock_path)
+        prv_path = Path(dataset_create.path)
+        # Build schema
+        schema_dict = dataset_create.model_dump()
+        schema_dict.pop("path")
+        schema_dict.pop("mock_path")
+        schema_dict.pop("description_path")
+        schema_dict["mock"] = DatasetUrlManager.get_syftbox_mock_dataset_url(
+            self._path_manager.syftbox_client_email, dataset_create.name, mock_path
+        )
+        schema_dict["private"] = DatasetUrlManager.get_syftbox_private_dataset_url(
+            dataset_create.name, prv_path
+        )
+        schema_dict["readme"] = DatasetUrlManager.get_syftbox_mock_dataset_url(
+            self._path_manager.syftbox_client_email,
+            dataset_create.name,
+            dataset_create.description_path,
+        )
+        # Write schema file
+        public_dataset_dir: Path = self._path_manager.get_syftbox_public_dataset_dir(
+            dataset_create.name
+        )
+        with open(public_dataset_dir / "dataset.schema.json", "w") as f:
+            json.dump(schema_dict, f, indent=2)
+
+
 class DatasetRDSClient(RDSClientModule):
     """
     For DatasetRDSClient, everything is done on the client side
-    Hence, there is no need for utilizing RPC Connections
+    Hence, there is no need for utilizing RPC Connections and the RPCClient
     """
 
     def __init__(self, config: RDSClientConfig, rpc_client: RPCClient):
         super().__init__(config, rpc_client)
+        self._config = config
         self._syftbox_client = SyftBoxClient.load()
+        self._path_manager = DatasetPathManager(self._syftbox_client)
+        self._schema_manager = DatasetSchemaManager(self._path_manager)
+        self._files_manager = DatasetFilesManager(self._path_manager)
 
     def raise_error_if_not_admin(self):
-        if self._syftbox_client.email != self.config.host:
+        if self._syftbox_client.email != self._config.host:
             raise ValueError(
                 f"SyftBox email and RDS host must be the same to create a dataset "
                 f"(no remote dataset creation for now). "
                 f"SyftBox email: {self._syftbox_client.email}. "
                 f"Host email: {self.config.host}"
             )
+
+    def _validate_before_create(
+        self,
+        name: str,
+        path: Union[str, Path],
+        mock_path: Union[str, Path],
+        file_type: str,
+    ) -> DatasetCreate:
+        if (
+            self._path_manager.get_syftbox_public_dataset_dir(name).exists()
+            or self._path_manager.get_syftbox_private_dataset_dir(name).exists()
+        ):
+            raise ValueError(f"Dataset with name '{name}' already exists")
+        # validate paths
+        self._path_manager.check_path_exists(path)
+        self._path_manager.check_path_exists(mock_path)
+        self._path_manager.check_are_both_dirs_or_files(path, mock_path)
+        # validate dataset file extensions
+        self._files_manager.check_same_file_extensions(path, mock_path)
+        self._files_manager.check_file_extensions_for_dir(path, file_type)
+        self._files_manager.check_file_extensions_for_dir(mock_path, file_type)
 
     def create(
         self,
@@ -43,23 +217,7 @@ class DatasetRDSClient(RDSClientModule):
         description_path: Optional[str] = None,
     ) -> Dataset:
         self.raise_error_if_not_admin()
-        if (
-            self.get_syftbox_public_dataset_dir(name).exists()
-            or self.get_syftbox_private_dataset_dir(name).exists()
-        ):
-            raise ValueError(f"Dataset with name '{name}' already exists")
-
-        # validate paths
-        check_path_exists(path)
-        check_path_exists(mock_path)
-        check_are_both_dirs_or_files(path, mock_path)
-
-        # validate dataset file extensions
-        check_same_file_extensions(path, mock_path)
-        check_file_extensions_for_dir(path, file_type)
-        check_file_extensions_for_dir(mock_path, file_type)
-
-        # validate input types
+        dataset_create = self._validate_before_create(name, path, mock_path, file_type)
         dataset_create = DatasetCreate(
             name=name,
             path=str(path),
@@ -70,12 +228,18 @@ class DatasetRDSClient(RDSClientModule):
         )
 
         try:
-            mock_syftbox_path = self._copy_mock_to_public_syftbox_dir(name, mock_path)
-            description_file_syftbox_path = self._copy_desc_file_to_public_syftbox_dir(
-                name, description_path
+            mock_syftbox_path = self._files_manager.copy_mock_to_public_syftbox_dir(
+                name, mock_path
             )
-            private_syftbox_path = self._copy_private_to_private_syftbox_dir(name, path)
-            self._generate_dataset_schema(dataset_create)
+            description_file_syftbox_path = (
+                self._files_manager.copy_desc_file_to_public_syftbox_dir(
+                    name, description_path
+                )
+            )
+            private_syftbox_path = (
+                self._files_manager.copy_private_to_private_syftbox_dir(name, path)
+            )
+            self._schema_manager.generate_dataset_schema(dataset_create)
 
             return Dataset(
                 name=name,
@@ -86,7 +250,7 @@ class DatasetRDSClient(RDSClientModule):
                 description_path=str(description_file_syftbox_path),
             )
         except Exception as e:
-            self._cleanup_dataset_files(name)
+            self._files_manager.cleanup_dataset_files(name)
             raise RuntimeError(f"Failed to create dataset '{name}': {str(e)}") from e
 
     def get(self) -> Dataset:
@@ -94,126 +258,6 @@ class DatasetRDSClient(RDSClientModule):
 
     def delete(self, name: str) -> None:
         try:
-            self._cleanup_dataset_files(name)
+            self._files_manager.cleanup_dataset_files(name)
         except Exception as e:
             raise RuntimeError(f"Failed to delete dataset '{name}': {str(e)}") from e
-
-    def _cleanup_dataset_files(self, name: str) -> None:
-        try:
-            public_dir = self.get_syftbox_public_dataset_dir(name)
-            private_dir = self.get_syftbox_private_dataset_dir(name)
-            shutil.rmtree(public_dir)
-            shutil.rmtree(private_dir)
-        except Exception as e:
-            logger.error(f"Failed to cleanup dataset files: {str(e)}")
-            raise RuntimeError(f"Failed to clean up dataset '{name}': {str(e)}") from e
-
-    def _copy_mock_to_public_syftbox_dir(
-        self, dataset_name: str, mock_path: Union[str, Path]
-    ) -> Path:
-        public_dataset_dir: Path = self.get_syftbox_public_dataset_dir(dataset_name)
-        public_dataset_dir.mkdir(parents=True, exist_ok=True)
-        mock_path = Path(mock_path)
-        dest_path = public_dataset_dir / mock_path.name
-        if mock_path.is_dir():
-            shutil.copytree(mock_path, dest_path, dirs_exist_ok=True)
-        else:
-            shutil.copy2(mock_path, dest_path)
-        return dest_path
-
-    def _copy_desc_file_to_public_syftbox_dir(
-        self, dataset_name: str, description_path: Union[str, Path]
-    ) -> Path:
-        public_dataset_dir: Path = self.get_syftbox_public_dataset_dir(dataset_name)
-        dest_path = public_dataset_dir / Path(description_path).name
-        shutil.copy2(description_path, public_dataset_dir / Path(description_path).name)
-        return dest_path
-
-    def get_syftbox_public_dataset_dir(self, dataset_name: str) -> Path:
-        return self._syftbox_client.my_datasite / "public" / "datasets" / dataset_name
-
-    def _copy_private_to_private_syftbox_dir(
-        self,
-        dataset_name: str,
-        path: Union[str, Path],
-    ) -> Path:
-        private_dataset_dir: Path = self.get_syftbox_private_dataset_dir(dataset_name)
-        private_dataset_dir.mkdir(parents=True, exist_ok=True)
-        prv_path = Path(path)
-        dest_path = private_dataset_dir / prv_path.name
-        if prv_path.is_dir():
-            shutil.copytree(prv_path, dest_path, dirs_exist_ok=True)
-        else:
-            shutil.copy2(prv_path, dest_path)
-        return dest_path
-
-    def get_syftbox_private_dataset_dir(self, dataset_name: str):
-        return (
-            self._syftbox_client.workspace.data_dir
-            / "private"
-            / "datasets"
-            / dataset_name
-        )
-
-    def _generate_dataset_schema(self, dataset_create: DatasetCreate) -> None:
-        mock_path = Path(dataset_create.mock_path)
-        prv_path = Path(dataset_create.path)
-        # Build schema
-        schema_dict = dataset_create.model_dump()
-        schema_dict.pop("path")
-        schema_dict.pop("mock_path")
-        schema_dict.pop("description_path")
-        schema_dict["mock"] = self.get_syftbox_mock_dataset_url(
-            dataset_create.name, mock_path
-        )
-        schema_dict["private"] = self.get_syftbox_private_dataset_url(
-            dataset_create.name, prv_path
-        )
-        schema_dict["readme"] = self.get_syftbox_mock_dataset_url(
-            dataset_create.name, dataset_create.description_path
-        )
-        # Write schema
-        public_dataset_dir: Path = self.get_syftbox_public_dataset_dir(
-            dataset_create.name
-        )
-        with open(public_dataset_dir / "dataset.schema.json", "w") as f:
-            json.dump(schema_dict, f, indent=2)
-
-    def get_syftbox_mock_dataset_url(
-        self, dataset_name: str, mock_path: Union[Path, str]
-    ) -> str:
-        return f"syft://{self._syftbox_client.email}/public/datasets/{dataset_name}/{Path(mock_path).name}"
-
-    def get_syftbox_private_dataset_url(
-        self, dataset_name: str, path: Union[Path, str]
-    ) -> str:
-        return f"syft://private/datasets/{dataset_name}/{Path(path).name}"
-
-
-def check_path_exists(path: Union[str, Path]):
-    if not Path(path).exists():
-        raise ValueError(f"Paths must exist: {path}")
-
-
-def check_are_both_dirs_or_files(path: Union[str, Path], mock_path: Union[str, Path]):
-    path, mock_path = Path(path), Path(mock_path)
-    if not (
-        (path.is_dir() and mock_path.is_dir())
-        or (path.is_file() and mock_path.is_file())
-    ):
-        raise ValueError(f"Paths must be same type: {path} and {mock_path}")
-
-
-def check_same_file_extensions(path: Union[str, Path], mock_path: Union[str, Path]):
-    path, mock_path = Path(path), Path(mock_path)
-    if (path.is_file() and mock_path.is_file()) and (
-        not path.suffix == mock_path.suffix
-    ):
-        raise ValueError(f"Files must have same extension: {path} and {mock_path}")
-
-
-def check_file_extensions_for_dir(path: Union[str, Path], file_type: str):
-    """
-    Check all the file extensions in the dir and compare if they are in the list of file extensions
-    """
-    pass
