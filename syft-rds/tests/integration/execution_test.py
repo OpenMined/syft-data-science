725cb8df3f56243354397e22af4178b3978ca7f9
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2025-02-27 14:26:18 +0100

    add bash runtime

--- syft-rds/tests/integration/execution_test.py
+++ syft-rds/tests/integration/execution_test.py
@@ -76,3 +76,78 @@ def test_job_execution(
     rds_client.rpc.jobs.update(job)
 
     job.share_artifacts()
+
+    # check the output and logs
+    assert (
+        test_dir / "assets/do/job_outputs" / str(job.name) / "output" / "my_result.csv"
+    ).exists()
+    assert (
+        test_dir / "assets/do/job_outputs" / str(job.name) / "logs" / "stdout.log"
+    ).exists()
+    assert (
+        test_dir / "assets/do/job_outputs" / str(job.name) / "logs" / "stderr.log"
+    ).exists()
+
+
+def test_bash_job_execution(
+    rds_client: RDSClient,
+    server_client: RDSClient,
+    yaml_store: Callable[[Type[BaseSpec]], YAMLFileSystemDatabase],
+):
+    # Client Side
+    test_dir = Path(__file__).parent
+    job = rds_client.jobs.submit(
+        user_code_path=test_dir / "assets/ds/ds.sh",
+    )
+
+    # Server Side
+    jobs = server_client.rpc.jobs.get_all(GetAllRequest())
+    assert len(jobs) == 1
+    assert jobs[0].uid == job.uid
+
+    job = jobs[0]
+
+    job.add_to_queue()
+
+    with pytest.raises(ValueError):
+        # can't share artifacts before the job is run
+        job.share_artifacts()
+
+    # Runner side
+    config = JobConfig(
+        # we use the parent directory of the user code path as the function folder
+        # and the name of the user code file as the args.
+        # the following commands are equivalent to each other:
+        # $ cd dir && python main.py
+        # $ cd job.user_code.path.parent && job.runtime job.user_code.path.name
+        function_folder=job.user_code.path.parent,
+        args=[job.user_code.path.name],
+        data_path=test_dir / "assets/do",
+        runtime="bash",
+        job_folder=test_dir / "assets/do/job_outputs" / str(job.name),
+    )
+
+    runner = DockerRunner(handlers=[FileOutputHandler(), RichConsoleUI()])
+
+    return_code = runner.run(
+        config,
+    )
+
+    # Update job status based on return code
+    job.status = (
+        JobStatus.job_run_finished if return_code == 0 else JobStatus.job_run_failed
+    )
+    rds_client.rpc.jobs.update(job)
+
+    job.share_artifacts()
+
+    # check the output and logs
+    assert (
+        test_dir / "assets/do/job_outputs" / str(job.name) / "output" / "my_result.csv"
+    ).exists()
+    assert (
+        test_dir / "assets/do/job_outputs" / str(job.name) / "logs" / "stdout.log"
+    ).exists()
+    assert (
+        test_dir / "assets/do/job_outputs" / str(job.name) / "logs" / "stderr.log"
+    ).exists()
