4bdfc51b67a7cb40fee4e52be41223dc2a34f6c2
Khoa Duy Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-07-01 15:08:10 +0700

    fixing hardcoded paths in `syft_rds.__init__.py`

--- packages/syft-rds/src/syft_rds/utils/paths.py
+++ packages/syft-rds/src/syft_rds/utils/paths.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 import syft_rds
 
-RDS_REPO_PATH = Path(syft_rds.__file__).parent.parent.parent
-RDS_NOTEBOOKS_PATH = RDS_REPO_PATH / "notebooks"
+SYFT_DATASCIENCE_REPO_PATH = Path(syft_rds.__file__).parents[4]
+SYFT_DATASCIENCE_NOTEBOOKS_PATH = SYFT_DATASCIENCE_REPO_PATH / "notebooks"
